8eb86d6fde6b2993f9497d7a2d237722611a4762
NEXUS <stealthnexus707@gmail.com>
2025-03-20 15:34:50 +0200

    Add files via upload

---
+++ Program/Config/Config.py
@@ -0,0 +1,24 @@
+# Copyright (c) RedTiger (https://redtiger.shop)
+# See the file 'LICENSE' for copying permission
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
+# EN: 
+#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
+#     - Do not resell this tool, do not credit it to yours.
+# FR: 
+#     - Ne pas toucher ni modifier le code ci-dessous. En cas d'erreur, veuillez contacter le propriétaire, mais en aucun cas vous ne devez toucher au code.
+#     - Ne revendez pas ce tool, ne le créditez pas au vôtre.
+
+name_tool       =  "PRONEX"
+type_tool       =  "Multi-Tools"
+version_tool    =  "6.3"
+coding_tool     =  "Python 3"
+language_tool   =  "EN"
+creator         =  "EXILEOMINOUS"
+platform        =  "Windows 10/11 & Linux"
+website         =  "minous.shop"
+github_tool     =  "github.com/PRONEX70733/NEXUS TOOLS"
+telegram        =  "t.me/nexus70733"
+url_config      =  "https://raw.githubusercontent.com/PRONEX70733/NEXUS-TOOLS/main/Program/Config/Config.py"
+url_downloads   =  "https://github.com/PRONEX70733/NEXUS-TOOLS/archive/main.zip"
+license         =  "https://github.com/PRONEX70733/NEXUS-TOOLS/blob/main/LICENSE"
+copyright       =  "Copyright (c) PRONEX70733 'GPL-3.0 license'"
